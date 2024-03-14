d6acd3b2528924695393caaf68076ee97f19632e
renatorrodrigues <renatorrodrigues2002@gmail.com>
2024-03-14 19:05:42 -0300

    fix styles

---
+++ components/navbar.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import { buttonVariants } from "./ui/button";
+import { Linkedin } from "lucide-react";
+
+export default function Navbar() {
+  return (
+    <nav className="flex flex-1 py-8 justify-between items-center">
+      <p className="text-lg">
+        my<span className="font-bold gradient-indigo-violet">.Snippets</span>
+      </p>
+
+      <Link
+        href="https://www.linkedin.com/in/renato-rrodrigues/"
+        target="_blank"
+        className={buttonVariants({ size: "icon" })}
+      >
+        <Linkedin />
+      </Link>
+    </nav>
+  );
+}
