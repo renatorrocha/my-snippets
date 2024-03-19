9b44403add027b1475eb4fa488f37a2760a271a3
renatorrodrigues <renatorrodrigues2002@gmail.com>
2024-03-19 17:53:41 -0300

    add github icon in navbar component

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,32 +1,44 @@
 import Link from "next/link";
 import { buttonVariants } from "./ui/button";
-import { Linkedin } from "lucide-react";
+import { Github, Linkedin } from "lucide-react";
 
 export default function Navbar() {
   return (
     <nav className="container sticky top-0 z-10 mb-8 flex justify-center border-b bg-white py-4">
       <div className="flex flex-1 items-center justify-between">
-        <Link href="/" className="transition-all duration-300 hover:scale-110">
+        <Link href="/" className="animate-3s-all hover:scale-110">
           <p className="text-lg">
             my
             <span className="gradient-indigo-violet font-bold">.Snippets</span>
           </p>
         </Link>
 
-        <Link
-          href="https://www.linkedin.com/in/renato-rrodrigues/"
-          target="_blank"
-          className={buttonVariants({ variant: "secondary" })}
-        >
-          <p className="hidden font-semibold md:block">
-            Linked{" "}
-            <span className="rounded bg-blue-500 px-1 py-0.5 text-white">
-              in
-            </span>
-          </p>
+        <section className="flex gap-4">
+          <Link
+            href="https://github.com/renatorrocha"
+            target="_blank"
+            className={buttonVariants({ variant: "secondary" })}
+          >
+            <p className="hidden font-semibold md:block">Github</p>
 
-          <Linkedin className="block fill-blue-500 text-blue-500 md:hidden " />
-        </Link>
+            <Github className="block fill-white text-black md:hidden " />
+          </Link>
+
+          <Link
+            href="https://www.linkedin.com/in/renato-rrodrigues/"
+            target="_blank"
+            className={buttonVariants({ variant: "secondary" })}
+          >
+            <p className="hidden font-semibold md:block">
+              Linked{" "}
+              <span className="rounded bg-blue-500 px-1 py-0.5 text-white">
+                in
+              </span>
+            </p>
+
+            <Linkedin className="block fill-blue-500 text-blue-500 md:hidden " />
+          </Link>
+        </section>
       </div>
     </nav>
   );
