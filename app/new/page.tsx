a693320b0f4eb3237733c5e1fceef2718f95582f
renatorrodrigues <renatorrodrigues2002@gmail.com>
2024-03-19 17:21:01 -0300

    feat: error message in create snipped form

--- app/new/page.tsx
+++ app/new/page.tsx
@@ -11,7 +11,11 @@ import { useForm } from "react-hook-form";
 import { toast } from "sonner";
 
 export default function NewSnippetPage() {
-  const SnippetForm = useForm<SnippetType>({
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<SnippetType>({
     resolver: zodResolver(SnippetSchema),
   });
 
@@ -21,19 +25,32 @@ export default function NewSnippetPage() {
   }
 
   return (
-    <section className="flex flex-col gap-4">
+    <section className="flex flex-1 flex-col gap-4">
       <h1 className="text-3xl font-bold text-primary">Create Snippet</h1>
       <form
-        onSubmit={SnippetForm.handleSubmit(onSubmit)}
+        onSubmit={handleSubmit(onSubmit)}
         className="flex flex-1 flex-col items-end gap-4"
       >
-        <Input placeholder="Title" {...SnippetForm.register("title")} />
-
-        <Textarea
-          placeholder="Type your code here."
-          className="resize-none"
-          {...SnippetForm.register("code")}
-        />
+        <div className="grid w-full gap-2">
+          <Input placeholder="Title" {...register("title")} />
+          {errors.title && (
+            <span className="ml-4 text-sm text-red-500 ">
+              {errors.title.message}
+            </span>
+          )}
+        </div>
+        <div className="grid w-full gap-2">
+          <Textarea
+            placeholder="Type your code here."
+            className="resize-none"
+            {...register("code")}
+          />
+          {errors.code && (
+            <span className="ml-4 text-sm text-red-500 ">
+              {errors.code.message}
+            </span>
+          )}
+        </div>
 
         <div className="flex gap-4">
           <Link href="/" className={buttonVariants({ variant: "outline" })}>
